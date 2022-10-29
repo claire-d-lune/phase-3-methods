2b987715583aa1c8a069f19ec35f42808a8c90ed
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-10-29 15:10:55 -0600

    Completed the lab.

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+
+
+def my_method param 
+    puts "Running my method"
+    param +1
+end
+
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+    name
+end
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (num1, num2) 
+    num1 + num2
+end
+
+def halve(num)
+    return nil unless num.class == Integer
+    num / 2
+  end
